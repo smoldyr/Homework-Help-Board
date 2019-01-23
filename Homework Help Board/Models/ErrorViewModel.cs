0cafaa288c5f85648d1380b48fa3ff151982a45e
Clark Bedsole <clarkb@clarkb.com>
2019-01-23 11:51:49 -0600

    Add project files.

---
+++ Homework Help Board/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Homework_Help_Board.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
