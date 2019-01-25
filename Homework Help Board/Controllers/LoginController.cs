454707e89607430eee8552ff8b36977243e2b5ca
Javan Rampersad <javan.rampersad@faulkner.edu>
2019-01-24 21:55:03 -0600

    Added in .cs file controllers for each page. Added in folders with .cshtml files for each page.

---
+++ Homework Help Board/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Homework_Help_Board.Models;
+
+namespace Homework_Help_Board.Controllers
+{
+	public class LoginController : Controller
+	{
+        
+	}
+}
