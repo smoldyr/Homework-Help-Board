dbd1550f3ed71115d62100c08f4dab554f7854d5
Javan Rampersad <javan.rampersad@faulkner.edu>
2019-02-04 19:18:41 -0600

    Commiting to pull

--- Homework Help Board/Startup.cs
+++ Homework Help Board/Startup.cs
@@ -30,7 +30,7 @@ namespace Homework_Help_Board
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
