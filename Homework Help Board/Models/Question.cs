89abae984014720982f525350888aeffc11d46ed
Javan Rampersad <javan.rampersad@summasource.com>
2019-02-08 09:47:18 -0600

    Reworked the controllers, removing many of the controllers and moving code to one controller to avoid duplicate code. Moved all views into one folder to work with the new, single controller.

---
+++ Homework Help Board/Models/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_Help_Board.Models
+{
+    public class Question : IPost<Question>
+    {
+        public int PostID { get; set; } //from IPost
+        public int UserID { get; set; } //from IPost
+        public DateTime DateCreated { get; set; } //from IPost
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public int Grade { get; set; }
+        public string Body { get; set; } //from IPost
+        public IList<Reply> Responses { get; set; }
+        public int NumFlags { get; set; } //from IPost
+    }
+}
---
+++ Homework Help Board/Models/Reply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_Help_Board.Models
+{
+    public class Reply : IPost<Reply>
+    {
+        public int PostID { get; set; } //from IPost
+        public int UserID { get; set; } //from IPost
+        public DateTime DateCreated { get; set; } //from IPost
+        public string Body { get; set; } //from IPost
+        public int NumVotes { get; set; }
+        public int NumFlags { get; set; } //from IPost
+    }
+}
