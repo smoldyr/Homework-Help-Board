7bc8307b6eeb2894e320be9551636db7786f992d
MsJovii <46581767+MsJovii@users.noreply.github.com>
2019-02-13 19:19:14 -0600

    Add View Model

---
+++ Homework Help Board/Models/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_Help_Board.Models
+
+{
+    public class ViewModel
+{
+
+        //Add the list here 
+
+
+}
+}
